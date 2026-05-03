f8d756e563fea984b552284c986b24298d259c00
kranthi-kumar-2004 <159746543+kranthi-kumar-2004@users.noreply.github.com>
2026-05-03 20:50:01 +0530

    Update ManageProducts.jsx

--- src/admin/components/ManageProducts.jsx
+++ src/admin/components/ManageProducts.jsx
@@ -8,7 +8,7 @@ function ManageProducts() {
     const [editId, setEditId] = useState(null);
     const [menuIndex, setMenuIndex] = useState(null);
     const API = import.meta.env.VITE_API_URL;
-console.log("API:", import.meta.env.VITE_API_URL);
+    
     const [form, setForm] = useState({
         name: "",
         price: "",
